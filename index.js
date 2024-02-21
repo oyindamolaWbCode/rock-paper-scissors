581ec50c2b9ed0172891d81ad79acf45abece79f
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2024-02-21 10:36:45 +0100

    modal added

---
+++ index.js
@@ -0,0 +1,12 @@
+let rules = document.querySelector('#rules');
+let span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("modal");
+
+rules.addEventListener('click', () =>{
+    modal.style.display = "block"; 
+})
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
